753b4446adcb34cdeb57e6dc8f50c044061f3e80
Makarov-AA <eglassus@gmail.com>
2018-10-15 13:14:52 +0300

    Wrong solution

--- makarov_aa/Practice/Practice2/Source.c
+++ makarov_aa/Practice/Practice2/Source.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int b[10];
 void main() {
 	const int k = RAND_MAX / 9;
-	int n, a = 0, i;
+	int n, a = 0, g, i;
 	do {
 		scanf("%d", &n);
-	} while (n <= 0);
+	} while ((n <= 0) || (n > 10));
 	srand((unsigned int)time(0));
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < 10; i++)
+		b[i] = i;
+	for (i = 0; i < 10; i++)
+
+		g = rand() * 9 / RAND_MAX;
+	/*for (i = 0; i < n; i++) {
 		a *= 10;
-		a += rand() * 9 / RAND_MAX;
+		g = rand() * 9 / RAND_MAX;
+		while (b[g] != 1) {
+			g = rand() * 9 / RAND_MAX;
+			b[g]++;
+		}
+		if (b[g] == 1)
+			a += g;
 	}
-	printf("%d", a);
+	printf("%d", a);*/
 }
