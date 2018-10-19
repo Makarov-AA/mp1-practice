310c6925d364ab8663474ab86de85e1fed282e37
Makarov-AA <eglassus@gmail.com>
2018-10-19 21:46:22 +0300

    111

---
+++ makarov_aa/Practice/Practice3/Source.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void main() {
+	int mode;//режима работы
+	printf("Выберите режим работы:\n1 - отгадывает пользователь\n2 - отгадывает компьютер");
+	do {
+		scanf("%d", &mode);
+		while ((mode != 1) || (mode != 2))
+	if (mode == )
+}
