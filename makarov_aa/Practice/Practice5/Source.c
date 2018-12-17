cc2109bfa9bab51014d32dbe6d2064bb1e30c2a4
Makarov-AA <43129398+Makarov-AA@users.noreply.github.com>
2018-12-17 10:44:21 +0300

    Update Source.c

--- makarov_aa/Practice/Practice5/Source.c
+++ makarov_aa/Practice/Practice5/Source.c
@@ -1,7 +1,50 @@
+//#include <windows.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#define MAX_LEN 256
+//int ListDirectoryContents(const wchar_t *sDir)
+//{
+//	WIN32_FIND_DATA fdFile;
+//	HANDLE hFind = NULL;
+//	wchar_t sPath[2048];
+//
+//	wsprintf(sPath, L"%s\\*.*", sDir);
+//	if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
+//	{
+//		wprintf(L"Path not found: [%s]\n", sPath);
+//		return 1;
+//	}
+//
+//	do
+//	{
+//		if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
+//		{
+//			ULONGLONG fileSize = fdFile.nFileSizeHigh;
+//			fileSize <<= sizeof(fdFile.nFileSizeHigh) * 8;
+//			fileSize |= fdFile.nFileSizeLow;
+//
+//			wsprintf(sPath, L"%s\\%s", sDir, fdFile.cFileName);
+//			wprintf(L"File: %s Size: %d\n", sPath, fileSize);
+//		}
+//	} while (FindNextFile(hFind, &fdFile));
+//	FindClose(hFind);
+//	return 0;
+//}
+//
+//void main()
+//{
+//	char* a = (char*)malloc(MAX_LEN);
+//	wchar_t* ca = (wchar_t*)malloc(MAX_LEN);
+//	fgets(a, MAX_LEN, stdin);
+//	a[strlen(a) - 1] = '\0';
+//	swprintf(ca, MAX_LEN, L"%hs", a);
+//	ListDirectoryContents(ca);
+//}
+
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_LEN 256
+
 int ListDirectoryContents(const wchar_t *sDir)
 {
 	WIN32_FIND_DATA fdFile;
@@ -9,6 +52,7 @@ int ListDirectoryContents(const wchar_t *sDir)
 	wchar_t sPath[2048];
 
 	wsprintf(sPath, L"%s\\*.*", sDir);
+	wprintf("%s", sPath);
 	if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
 	{
 		wprintf(L"Path not found: [%s]\n", sDir);
@@ -33,10 +77,5 @@ int ListDirectoryContents(const wchar_t *sDir)
 
 void main()
 {
-	char* a = (char*)malloc(MAX_LEN);
-	wchar_t* ca = (wchar_t*)malloc(MAX_LEN);
-	fgets(a, MAX_LEN, stdin);
-	a[strlen(a) - 1] = '\0';
-	swprintf(ca, MAX_LEN, L"%hs", a);
-	ListDirectoryContents(ca);
+	ListDirectoryContents(L"E:\\stack\\");
 }
