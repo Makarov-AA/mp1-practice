9604988e306a2d8de0928aa4280bfab8b025ecf8
Makarov-AA <43129398+Makarov-AA@users.noreply.github.com>
2018-12-24 10:40:33 +0300

    commit practice5

--- makarov_aa/Practice/Practice5/Source.c
+++ makarov_aa/Practice/Practice5/Source.c
@@ -1,58 +1,16 @@
-//#include <windows.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#define MAX_LEN 256
-//int ListDirectoryContents(const wchar_t *sDir)
-//{
-//	WIN32_FIND_DATA fdFile;
-//	HANDLE hFind = NULL;
-//	wchar_t sPath[2048];
-//
-//	wsprintf(sPath, L"%s\\*.*", sDir);
-//	if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
-//	{
-//		wprintf(L"Path not found: [%s]\n", sPath);
-//		return 1;
-//	}
-//
-//	do
-//	{
-//		if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
-//		{
-//			ULONGLONG fileSize = fdFile.nFileSizeHigh;
-//			fileSize <<= sizeof(fdFile.nFileSizeHigh) * 8;
-//			fileSize |= fdFile.nFileSizeLow;
-//
-//			wsprintf(sPath, L"%s\\%s", sDir, fdFile.cFileName);
-//			wprintf(L"File: %s Size: %d\n", sPath, fileSize);
-//		}
-//	} while (FindNextFile(hFind, &fdFile));
-//	FindClose(hFind);
-//	return 0;
-//}
-//
-//void main()
-//{
-//	char* a = (char*)malloc(MAX_LEN);
-//	wchar_t* ca = (wchar_t*)malloc(MAX_LEN);
-//	fgets(a, MAX_LEN, stdin);
-//	a[strlen(a) - 1] = '\0';
-//	swprintf(ca, MAX_LEN, L"%hs", a);
-//	ListDirectoryContents(ca);
-//}
-
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
-int ListDirectoryContents(const wchar_t *sDir)
+int ListDirectoryContents(const wchar_t *sDir, ULONGLONG* size, wchar_t** file_name)
 {
+	int i;
 	WIN32_FIND_DATA fdFile;
 	HANDLE hFind = NULL;
 	wchar_t sPath[2048];
 
 	wsprintf(sPath, L"%s\\*.*", sDir);
-	wprintf("%s", sPath);
 	if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
 	{
 		wprintf(L"Path not found: [%s]\n", sDir);
@@ -66,7 +24,8 @@ int ListDirectoryContents(const wchar_t *sDir)
 			ULONGLONG fileSize = fdFile.nFileSizeHigh;
 			fileSize <<= sizeof(fdFile.nFileSizeHigh) * 8;
 			fileSize |= fdFile.nFileSizeLow;
-
+			size[i] = fileSize;
+			wchar_t[i] = 
 			wsprintf(sPath, L"%s\\%s", sDir, fdFile.cFileName);
 			wprintf(L"File: %s Size: %d\n", sPath, fileSize);
 		}
@@ -74,8 +33,36 @@ int ListDirectoryContents(const wchar_t *sDir)
 	FindClose(hFind);
 	return 0;
 }
+void bubble(int* a, int n)
+{
+	int i = 0, j, tmp;
+	for (; i < n; i++)
+		for (j = 0; j < n - i - 1; j++)
+			if (a[j] > a[j + 1])
+			{
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+
+}
 
 void main()
 {
-	ListDirectoryContents(L"E:\\stack\\");
+	struct file_data
+	{
+		ULONGLONG* size;
+		wchar_t** file_name;
+	} data;
+	int sort_num;
+	setlocale(LC_ALL, "Russian");
+	printf("Введите путь");
+	ListDirectoryContents(L"E:\\stack\\", data.size, data.file_name);
+	printf("Выберите метод сортировки:\n1: Пузырьком\n2:Выбором\n3:Вставками\n");
+	scanf("%d", &sort_num);
+	switch (sort_num)
+	{
+	case 1:
+
+	} 
 }
