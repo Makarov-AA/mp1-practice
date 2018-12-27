62afd8b1973fa43b334dec80ca4639153828b01b
Makarov-AA <eglassus@gmail.com>
2018-12-27 05:23:36 +0300

    Завершено

--- makarov_aa/Practice/Practice5/Source.c
+++ makarov_aa/Practice/Practice5/Source.c
@@ -3,66 +3,281 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int ListDirectoryContents(const wchar_t *sDir, ULONGLONG* size, wchar_t** file_name)
+#define N 1000
+#define MAX_LEN 2048
+
+int ListDirectoryContents(const wchar_t *sDir, ULONGLONG* size, wchar_t** file_name, wchar_t** temp)
 {
-	int i;
-	WIN32_FIND_DATA fdFile;
-	HANDLE hFind = NULL;
-	wchar_t sPath[2048];
+    int i = 0;
+    WIN32_FIND_DATA fdFile;
+    HANDLE hFind = NULL;
+    wchar_t sPath[MAX_LEN];
 
-	wsprintf(sPath, L"%s\\*.*", sDir);
-	if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
-	{
-		wprintf(L"Path not found: [%s]\n", sDir);
-		return 1;
-	}
+    wsprintf(sPath, L"%s\\*.*", sDir);
+    if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
+    {
+        wprintf(L"Path not found: [%s]\n", sDir);
+        return 1;
+    }
 
-	do
-	{
-		if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
-		{
-			ULONGLONG fileSize = fdFile.nFileSizeHigh;
-			fileSize <<= sizeof(fdFile.nFileSizeHigh) * 8;
-			fileSize |= fdFile.nFileSizeLow;
-			size[i] = fileSize;
-			wchar_t[i] = 
-			wsprintf(sPath, L"%s\\%s", sDir, fdFile.cFileName);
-			wprintf(L"File: %s Size: %d\n", sPath, fileSize);
-		}
-	} while (FindNextFile(hFind, &fdFile));
-	FindClose(hFind);
-	return 0;
-}
-void bubble(int* a, int n)
-{
-	int i = 0, j, tmp;
-	for (; i < n; i++)
-		for (j = 0; j < n - i - 1; j++)
-			if (a[j] > a[j + 1])
-			{
-				tmp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = tmp;
-			}
-
+    do
+    {
+        if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
+        {
+            file_name[i] = (wchar_t*)malloc(sizeof(wchar_t*) * MAX_LEN);
+            temp[i] = (wchar_t*)malloc(sizeof(wchar_t*) * MAX_LEN);
+            ULONGLONG fileSize = fdFile.nFileSizeHigh;
+            fileSize <<= sizeof(fdFile.nFileSizeHigh) * 8;
+            fileSize |= fdFile.nFileSizeLow;
+            size[i] = fileSize;
+            wsprintf(file_name[i], L"%s\\%s", sDir, fdFile.cFileName);
+            wprintf(L"File: %s Size: %d\n", file_name[i] , fileSize);
+            i++;
+        }
+    } while (FindNextFile(hFind, &fdFile));
+    FindClose(hFind);
+    return 1;
 }
 
+int CountofFiles(const wchar_t *sDir)
+{
+    int i = 0;
+    WIN32_FIND_DATA fdFile;
+    HANDLE hFind = NULL;
+    wchar_t sPath[2048];
+
+    wsprintf(sPath, L"%s\\*.*", sDir);
+    if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
+    {
+        wprintf(L"Path not found: [%s]\n", sDir);
+        return -1;
+    }
+
+    do
+    {
+        if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
+        {
+            i++;
+        }
+    } while (FindNextFile(hFind, &fdFile));
+    FindClose(hFind);
+    return i;
+}
+
+void choose(ULONGLONG* a, wchar_t** file_name, int n)
+{
+    int i = 0, j, min, minindx;
+    wchar_t** tmp;
+    for (; i < n - 1; i++)
+    {
+        min = a[i];
+        minindx = i;
+        for (j = i + 1; j < n; j++)
+            if (a[j] < min)
+            {
+                min = a[j];
+                minindx = j;
+            }
+        a[minindx] = a[i];
+        a[i] = min;
+        tmp = file_name[minindx];
+        file_name[minindx] = file_name[i];
+        file_name[i] = tmp;
+    }
+}
+
+void insert(ULONGLONG* a, wchar_t** f, int n)
+{
+    int i, j;
+    ULONGLONG tmp;
+    wchar_t* temp;
+    for (i = 1; i < n; i++)
+    {
+        tmp = a[i];
+        j = i - 1;
+        while ((j >= 0) && (a[j] > tmp))
+        {
+            a[j + 1] = a[j];
+            a[j] = tmp;
+            temp = f[j + 1];
+            f[j + 1] = f[j];
+            f[j] = temp;
+            j--;
+        }
+    }
+}
+
+void bubble(ULONGLONG* a, wchar_t** f, int n)
+{
+    int i = 0, j;
+    ULONGLONG tmp;
+    wchar_t* temp;
+    for (; i < n; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (a[j] > a[j + 1])
+            {
+                tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+                temp = f[j];
+                f[j] = f[j + 1];
+                f[j + 1] = temp;
+            }
+
+}
+
+void quicksplit(ULONGLONG* a, wchar_t** f, int* i, int* j, int p)
+{
+    do {
+        while (a[*i] < p) (*i)++;
+        while (a[*j] > p) (*j)--;
+        if (*i <= *j)
+        {
+            ULONGLONG tmp = a[*i];
+            wchar_t* temp = f[*i];
+            a[*i] = a[*j];
+            a[*j] = tmp;
+            f[*i] = f[*j];
+            f[*j] = temp;
+            (*i)++;
+            (*j)--;
+        }
+    } while (*i <= *j);
+}
+
+void quicksort(ULONGLONG* a, wchar_t** f, int n1, int n2)
+{
+    if (n1 < n2)
+    {
+        int pidx = (n1 + n2) / 2;
+        int i = n1, j = n2;
+        quicksplit(a, f, &i, &j, a[pidx]);
+        quicksort(a, f, n1, j);
+        quicksort(a, f, i, n2);
+    }
+}
+
+void merge(ULONGLONG* a, ULONGLONG* buffer, wchar_t** f, wchar_t** temp, int l, int m, int r)
+{
+    int i, j, k;
+    i = l; j = m + 1; k = l;
+    while (k <= r)
+    {
+        while (i <= m && j <= r)
+            if (a[i] < a[j]) 
+            {
+                buffer[k] = a[i];
+                temp[k] = f[i];
+                k++; i++;
+            }
+
+            else
+                if (a[j] < a[i] && j <= r)
+                {
+                    buffer[k] = a[j];
+                    temp[k] = f[j];
+                    k++; j++;
+                }
+                else
+                    if (a[i] == a[j])
+                    {
+                        buffer[k] = a[i];
+                        temp[k] = f[i];
+                        k++; i++;
+                        buffer[k] = a[j];
+                        temp[k] = f[j];
+                        k++; j++;
+                    }
+        while (i <= m)
+        {
+            buffer[k] = a[i];
+            temp[k] = f[i];
+            k++; i++;
+        }
+        while (j <= r)
+        {
+            buffer[k] = a[j];
+            temp[k] = f[j];
+            k++; j++;
+        }
+        for (i = l; i <= r; i++) 
+        {
+            a[i] = buffer[i];
+            f[i] = temp[i];
+        }
+
+    }
+}
+
+void mergesort(ULONGLONG* a, ULONGLONG* buffer, wchar_t** f, wchar_t** temp, int l, int r)
+{
+    if (l >= r) {
+        return;
+    }
+    int m = (l + r) / 2;
+    mergesort(a, buffer, f, temp, l, m);
+    mergesort(a, buffer, f, temp, m + 1, r);
+    merge(a, buffer, f, temp, l, m, r);
+}
 void main()
-{
-	struct file_data
-	{
-		ULONGLONG* size;
-		wchar_t** file_name;
-	} data;
-	int sort_num;
-	setlocale(LC_ALL, "Russian");
-	printf("Введите путь");
-	ListDirectoryContents(L"E:\\stack\\", data.size, data.file_name);
-	printf("Выберите метод сортировки:\n1: Пузырьком\n2:Выбором\n3:Вставками\n");
-	scanf("%d", &sort_num);
-	switch (sort_num)
-	{
-	case 1:
+{ 
+    setlocale(LC_ALL, "Russian");
+    int n, i, sort_num, check;
+    ULONGLONG* tmp = NULL;
+    ULONGLONG* size = NULL;
+    wchar_t* file_name[N] = { NULL };
+    wchar_t* temp[N] = { NULL };
+    char* a = (char*)malloc(sizeof(char) * MAX_LEN);
+    wchar_t* ca = (wchar_t*)malloc(sizeof(wchar_t) * MAX_LEN);
+    printf("Р’РІРµРґРёС‚Рµ РїСѓС‚СЊ\n");
+    fgets(a, MAX_LEN, stdin);
+    a[strlen(a) - 1] = '\0';
+    swprintf(ca, MAX_LEN, L"%hs", a);
+    n = CountofFiles(ca);
+    if (n == -1)
+        return 1;
+    size = (ULONGLONG*)malloc(sizeof(ULONGLONG*) * n);
+    tmp = (ULONGLONG*)malloc(sizeof(ULONGLONG*) * n);
+    ListDirectoryContents(ca, size, file_name, temp);
+    for (i = 0; i < n; i++)
+    {
+        tmp[i] = size[i];
+        temp[i] = file_name[i];
+    }
+    printf("Р’С‹Р±РµСЂРёС‚Рµ РјРµС‚РѕРґ СЃРѕСЂС‚РёСЂРѕРІРєРё:\n1: Р’С‹Р±РѕСЂРѕРј\n2: Р’СЃС‚Р°РІРєР°РјРё\n3: РџСѓР·С‹СЂСЊРєРѕРј\n4: Р‘С‹СЃС‚СЂР°СЏ\n5: РЎР»РёСЏРЅРёРµРј\n0: Р’С‹С…РѕРґ\n");
+    scanf("%d", &sort_num);
+    while (sort_num != 0) {
+        switch (sort_num)
+        {
+        case 1:
+            choose(size, file_name, n);
+            for (i = 0; i < n; i++)
+                wprintf(L"File: %s Size: %d\n", file_name[i], size[i]);
+            break;
+        case 2:
+            insert(size, file_name, n);
+            for (i = 0; i < n; i++)
+                wprintf(L"File: %s Size: %d\n", file_name[i], size[i]);
+            break;
+        case 3:
+            bubble(size, file_name, n);
+            for (i = 0; i < n; i++)
+                wprintf(L"File: %s Size: %d\n", file_name[i], size[i]);
+            break;
+        case 4:
+            quicksort(size, file_name, 0, n);
+            for (i = 0; i < n; i++)
+                wprintf(L"File: %s Size: %d\n", file_name[i], size[i]);
+            break;
+        case 5:
+            mergesort(size, tmp, file_name, temp, 0, n);
+            for (i = 0; i < n; i++)
+                wprintf(L"File: %s Size: %d\n", file_name[i], size[i]);
+            break;
+        }
+        scanf("%d", &sort_num);
+    }
 
-	} 
+
+    
 }
